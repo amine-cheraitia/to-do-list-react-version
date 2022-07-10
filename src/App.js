86a7d5fb6b6eb990a1b351736a8e2b40f7f01812
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-07-10 03:27:46 +0200

    100%

--- src/App.js
+++ src/App.js
@@ -2,24 +2,52 @@ import "../node_modules/bulma/css/bulma.min.css";
 import "./App.css";
 import Header from "./Component/Header/Header";
 import Card from "./Component/Card/Card";
+import { useState } from "react";
+
 function App() {
+	const [monState, setMonState] = useState([
+		{ tache: "tache 1", txt: "random text" },
+		{ tache: "tache 2", txt: "random text 2" },
+		{ tache: "tache 3", txt: "random text 3" },
+	]);
+	const [tache, setTache] = useState();
+	const [txt, setTxt] = useState();
+
+	function creationCarte(e) {
+		e.preventDefault();
+		const nvTab = [...monState, { tache: tache, txt: txt }];
+		setTache("");
+		setTxt("");
+		setMonState(nvTab);
+	}
+
+	function suppCarte(index) {
+		const tab = [...monState];
+		const newState = tab.filter(
+			(elem) => tab.indexOf(elem) !== tab.indexOf(tab[index])
+		);
+
+		setMonState(newState);
+	}
 	return (
 		<div>
 			<Header />
 			<div className="container px-3">
 				<h2 className="title mt-5">Rentrez vos tâches à faire</h2>
 
-				<form>
+				<form onSubmit={creationCarte}>
 					<div className="field">
 						<div className="control">
 							<label htmlFor="tache" className="label">
 								Tâche
 							</label>
 							<input
+								value={tache}
 								className="input"
 								type="text"
 								id="tache"
 								placeholder="une tache a faire"
+								onChange={(e) => setTache(e.target.value)}
 							/>
 						</div>
 					</div>
@@ -29,10 +57,12 @@ function App() {
 								Contenu de la tâche
 							</label>
 							<textarea
+								value={txt}
 								className="textarea"
 								type="text"
 								id="txt"
 								placeholder="une tache a faire"
+								onChange={(e) => setTxt(e.target.value)}
 							></textarea>
 						</div>
 					</div>
@@ -42,7 +72,17 @@ function App() {
 						</button>
 					</div>
 				</form>
-				<Card />
+
+				{monState.map((todo, index) => {
+					return (
+						<Card
+							tache={todo.tache}
+							index={index}
+							txt={todo.txt}
+							deleteCarte={suppCarte}
+						/>
+					);
+				})}
 			</div>
 		</div>
 	);
--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({ tache, txt, deleteCarte, index }) {
 	return (
 		<div className="card has-background-info my-4">
 			<div className="card-content">
 				<div className="content">
-					<h3 className="px-4">Tâche :</h3>
-					<p className="is-size-4 px-4">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-						id.
-					</p>
-					<button className="delete is-large btn-top">X</button>
+					<h3 className="px-4">{tache} :</h3>
+					<p className="is-size-4 px-4">{txt}</p>
+					<button
+						className="delete is-large btn-top"
+						onClick={() => deleteCarte(index)}
+					>
+						X
+					</button>
 				</div>
 			</div>
 		</div>
